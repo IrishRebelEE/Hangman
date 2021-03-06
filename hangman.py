7259c661a394379eac6e9a186b3745c3094fa3df
IrishRebelEE <joebonath@gmail.com>
2021-03-05 17:17:28 -0800

    First code for hangman program

---
+++ hangman.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 12:58:59 2021
+
+@author: joe
+"""
+
+def hangman():
+    import random
+    
+    word_list = ["cat","dog","fish","utopia"]
+    word_index = random.randint(0,len(word_list)-1)
+    word = word_list[word_index]
+    
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n"
+              .join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+hangman()
